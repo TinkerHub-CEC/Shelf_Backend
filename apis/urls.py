26483702fa2fdb81ef1a14f13beeed445e4b4272
Paul T George <tgpaul9801@gmail.com>
2021-06-07 18:58:03 +0530

    Initial Commit

--- apis/urls.py
+++ apis/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('events/<int:id>', views.event_detail),
     path('events/<int:id>/registrations/', views.event_registrations),
     path('events/<int:id>/register/', views.register_for_event),
+    path('users/',views.user_list),
+    path('users/<int:id>/',views.user_details),
 ]
