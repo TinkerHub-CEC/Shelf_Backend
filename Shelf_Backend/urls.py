f3975a1a9833f798c70e3ddd74658f5e1663d193
razabinashraf <64091576+razabinashraf@users.noreply.github.com>
2021-08-29 12:35:08 +0530

    password  reset option added

--- Shelf_Backend/urls.py
+++ Shelf_Backend/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path,include
 from django_email_verification import urls as email_urls
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
     path('email/', include(email_urls)),
-    
+
+    path("password_reset/", views.password_reset_request, name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'), name='password_reset_complete'), 
 ]
 from django.conf import settings
 from django.conf.urls.static import static
