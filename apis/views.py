7c784cbeec1340b62a0e7240e08ba810b800286c
razabinashraf <64091576+razabinashraf@users.noreply.github.com>
2021-09-08 22:12:28 +0530

    changes in cron job

--- apis/views.py
+++ apis/views.py
@@ -219,7 +219,7 @@ def mark_attendance(request,id):
             user = request.POST.get('user')
             reg_obj = EventRegistration.objects.get(user=user, event=id)
         except EventRegistration.DoesNotExist:
-            return Response({'dev_data': f'Event Registration object with event id={id} and user id= {user} Doesnot Exist!', 'app_data': 'Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'dev_data': f'Event Registration object with event id={id} and user id= {user} Doesnot Exist!', 'app_data': f'Something went wrong!', 'attendance': request.POST.get('attendance')}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = EventRegistrationSerializer(reg_obj, data=request.data, partial=True)
         if serializer.is_valid():
@@ -338,10 +338,16 @@ def active_registrations_with_attendance(request,format=None):
 
     try: 
         if request.method == 'GET':
-            active_registrations = Event.objects.filter(reg_open_date__lt=datetime.now(),reg_close_date__gt=datetime.now())
-            active_with_attendance = active_registrations.exclude(attendance_method = 0)
+            # active_registrations = Event.objects.filter(reg_open_date__lt=datetime.now(),reg_close_date__gt=datetime.now())
+            # active_with_attendance = active_registrations.exclude(attendance_method = 0)
+            # serializer = EventSerializer(active_with_attendance, many=True)
+            # return Response(serializer.data) 
+
+            active_registrations = EventRegistration.objects.filter(user=request.user,event__reg_open_date__lt=datetime.now(),event__reg_close_date__gt=datetime.now())
+            active_with_attendance = active_registrations.event.all().exclude(attendance_method = 0)
             serializer = EventSerializer(active_with_attendance, many=True)
-            return Response(serializer.data) 
+            return Response(serializer.data)
+
     
     except Exception as e: 
         return Response({'dev_data': str(e), 'app_data': 'Something went wrong!'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
